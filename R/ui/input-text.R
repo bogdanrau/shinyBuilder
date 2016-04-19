0fb1ec54b7fc8dd5ae88d4ec2fee84a67380ce75
bogdanrau <bogdan.rau@gmail.com>
2016-04-18 17:49:51 -0700

    added closing quotes to fix menu page

--- R/ui/input-text.R
+++ R/ui/input-text.R
@@ -13,23 +13,23 @@ miniUI::miniContentPanel(
         flex = c(2,6)
       ),
       shiny::fillRow(
-        shiny::fillCol(shiny::HTML("<b>Input Label:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='Display label for the control. Can be blank for no label.")),
+        shiny::fillCol(shiny::HTML("<b>Input Label:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='Display label for the control. Can be blank for no label.'></i>")),
         shiny::fillCol(shiny::textInput("inputTextLabel", label = NULL, value = NULL, width = "100%")),
         flex = c(2,6)
       ),
       shiny::fillRow(
-        shiny::fillCol(shiny::HTML("<b>Initial Value:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='Initial value. Can be left blank.")),
+        shiny::fillCol(shiny::HTML("<b>Initial Value:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='Initial value. Can be left blank.'></i>")),
         shiny::fillCol(shiny::textInput("inputTextInitialValue", label = NULL, value = NULL, width = "100%")),
         flex = c(2,6)
       ),
       shiny::fillRow(
-        shiny::fillCol(shiny::HTML("<b>Width:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='The width of the input. eg. 400px or 100%.")),
+        shiny::fillCol(shiny::HTML("<b>Width:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='The width of the input. eg. 400px or 100%.'></i>")),
         shiny::fillCol(shiny::textInput("inputTextWidth", label = NULL, value = NULL, width = "100%")),
         flex = c(2,6)
       )
       ,
       shiny::fillRow(
-        shiny::fillCol(shiny::HTML("<b>Placeholder:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='A character string giving the user a hint as to what can be entered into the control. Internet Explorer 8 and 9 do not support this option.")),
+        shiny::fillCol(shiny::HTML("<b>Placeholder:</b> <i class='fa fa-info-circle' data-toggle='tooltip' data-placement='right' title='A character string giving the user a hint as to what can be entered into the control. Internet Explorer 8 and 9 do not support this option.'></i>")),
         shiny::fillCol(shiny::textInput("inputTextPlaceholder", label = NULL, value = NULL, width = "100%")),
         flex = c(2,6)
       )
